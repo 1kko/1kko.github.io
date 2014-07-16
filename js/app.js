3f69aef884996d721c544f1964d8584be79b64c4
ikko <ikko@ahnlab.com>
2014-07-17 00:52:29 +0900

    changed eatout structure, reading from RestaurantsData. Menus are also displayed.

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ App.Router.map(function()
 	this.resource('parking');
 	this.resource('about');
 	this.resource('eatout');
+	this.resource('restaurant', { path: ':Rid'});
 	this.resource('laddergame');
 });
 
@@ -55,11 +56,12 @@ App.MenuRoute = Ember.Route.extend({
 	}
 });
 
-Ember.Handlebars.helper('format-date', function(date){
-	var d = new Date(date);
-	var week = new Array('일', '월', '화', '수', '목', '금', '토');
-	var str=(week[d.getDay()]);
-	return new Ember.Handlebars.SafeString(date+' ('+str+')');
+App.RestaurantRoute = Ember.Route.extend({
+	model: function(params) {
+		var retVal=RestaurantsData.findBy('Rid', params.Rid);
+		console.debug(retVal)
+		return retVal;
+	}
 });
 
 function getMenusData(Url) {
@@ -68,6 +70,192 @@ function getMenusData(Url) {
 	var jsondata=$.parseJSON(retval);
 	return jsondata;
 };
+
+var RestaurantsData=[
+	{ 
+		Rid:"1", 
+		title: "명인만두", 
+		tel:"0316967433", 
+		img:"img/img_myoungin.png", 
+		desc:[
+			{line:"평일 07:30~21:00 / 일요일/공휴일 07:30~19:30 / 토요일: 휴무"}, 
+			{line:"위치: H스퀘어"}, 
+			{line:"메뉴: 분식류(6000원 한도)"}
+		],
+		menulist:[
+			{item:"고기만두", price:"3,500"}, 
+			{item:"김치만두", price:"3,500"}, 
+			{item:"고기+김치", price:"	3,500"}, 
+			{item:"군만두", price:"4,000"}, 
+			{item:"모듬만두", price:"6,000"}, 
+			{item:"고추만두", price:"4,000"}, 
+			{item:"수제튀김만두", price:"3,000"}, 
+			{item:"떡튀범벅", price:"5,500"}, 
+			{item:"비빔만두", price:"6,000"}, 
+			{item:"(떡)만두국", price:"6,000"},	 
+			{item:"만두칼국수", price:"6,000"}, 
+			{item:"얼큰만두국", price:"6,000"}, 
+			{item:"만두잡채덮밥", price:"6,000"}, 
+			{item:"고추김밥", price:"3,500"}, 
+			{item:"새우김밥", price:"3,500"}, 
+			{item:"참치김밥", price:"3,500"}, 
+			{item:"모듬김밥", price:"3,500"}, 
+			{item:"야채김밥", price:"2,500"}, 
+			{item:"치즈김밥", price:"3,000"}, 
+			{item:"오뎅(1인분", price:"2,000"}, 
+			{item:"명인떡볶이", price:"3,500"}, 
+			{item:"라면사리,오뎅, 계란, 만두", price:"각 1,000"}, 
+			{item:"항아리수제비", price:"6,000"}, 
+			{item:"짜장라볶이", price:"4,500"}, 
+			{item:"해물볶음우동", price:"6500 ->6,000"},
+			{item:"비빔국수", price:"5,500"}, 
+			{item:"명인라면", price:"3,000"}, 
+			{item:"떡사리, 만두, 김치, 치즈", price:"각 500"},
+			{item:"해장라면", price:"4,500"}, 
+			{item:"물냉면", price:"5,000"}, 
+			{item:"비빔냉면", price:"5,000"}, 
+			{item:"쫄면", price:"	5,000"}, 
+			{item:"해물오믈렛", price:"6,000"}, 
+			{item:"김치새우볶음밥", price:"6,000"}, 
+			{item:"김치참치볶음밥", price:"6,000"}, 
+			{item:"명인볶음밥", price:"6,000"}, 
+			{item:"김치(참치)찌개", price:"5,000"},	
+			{item:"순두부찌개", price:"5,000"}, 
+			{item:"돈까스", price:"6,000"}, 
+			{item:"매콤치킨까스", price:"6,500 -> 6,000"},
+			{item:"고구마치즈돈까스", price:"6,500 -> 6,000"},
+			{item:"된장찌개", price:"5,000"}, 
+			{item:"제육덮밥", price:"6,500 -> 6,000 "},
+			{item:"오징어덮밥", price:"6,000"}, 
+			{item:"비빔밥", price:"5,000"}, 
+			{item:"돌솥비빔밥", price:"6,000"}, 
+			{item:"김치날치알돌솥밥", price:"6,000"}, 
+			{item:"공기밥", price:"1,000"}, 
+			{item:"모닝SET(오전11시까지) 라면+김밥+계란후라이", price:"5,000"},
+			{item:"냉쫄면", price:"5,000"}, 
+			{item:"냉모밀만두정식", price:"6,000"}, 
+			{item:"콩국수", price:"6,000"},
+		]
+	},
+	{ 
+		Rid:"2", 
+		title: "찌개애감동", 
+		tel:"0316281479", 
+		img:"img/img_jjigae", 
+		desc:[
+			{line:"평일 10:30~22:00 / 토요일/공휴일 10:30~15:00 / 일요일: 휴무"}, 
+			{line:"위치: 유스페이스"},
+			{line:"메뉴: 찌개류, 제육불고기"}
+		],
+		menulist:[
+			{item:"차돌박이된장찌개", price:"7,000 -> 6,000"},
+			{item:"시골된장찌개", price:"6,000"},
+			{item:"옛날청국장찌개", price:"6,000"},
+			{item:"해물순두부찌개", price:"6,000"},
+			{item:"추억愛고추장찌개", price:"6,000"},
+			{item:"참치김치찌개", price:"6,000"},
+			{item:"옹기비빔밥", price:"6,000"},
+			{item:"뚝불고기정식", price:"7,000 -> 6,000"},
+			{item:"모짜렐라치즈계란말이", price:"6,000"},
+			{item:"계란탕", price:"6,000"},
+			{item:"제육불고기", price:"6,000"},
+			{item:"생선구이(단품)", price:"6,000"},
+		]
+	},
+	{ 
+		Rid:"3", 
+		title: "담소사골순대", 
+		tel:"0317893821", 
+		img:"img/img_damso.png", 
+		desc:[
+			{line:"평일 24시간 / 월요일 11:00 Open / 일요일: 22:30 Close"}, 
+			{line:"위치: H스퀘어"}, 
+			{line:"메뉴: 메뉴: 우사골돈순대국\우사골우순대국"}
+		],
+		menulist:[
+			{item:"우사골돈순대국", price:"5,700"},
+			{item:"우사골우순대국", price:"5,900"},
+		]
+	},
+	{ 
+		Rid:"4", 
+		title: "the진국", 
+		tel:"0317893555", 
+		img:"img/img_jincook.png", 
+		desc:[
+			{line:"평일/토요일/공휴일 24시간 / 일요일: 22:30 Close"}, 
+			{line:"위치: H스퀘어 S동"}, 
+			{line:"메뉴: 수육국밥"}
+		],
+		menulist:[
+			{item:"수육국밥", price:"6,000"}, 	
+		]
+	},
+	{ 
+		Rid:"5", 
+		title: "신기소", 
+		tel:"0317893737", 
+		img:"img/img_singiso.png", 
+		desc:[
+			{line:"평일/주말,공휴일 11:00~21:00"}, 
+			{line:"위치: H스퀘어 S동"}, 
+			{line:"메뉴: 가쯔돈, 김치가쯔돈, 우동, 돌솥알밥, 김치우동, 회덥밥, 새우튀김우동)"}
+		],
+		menulist:[
+			{item:"가쯔돈", price:"7,500 -> 6000"},
+			{item:"김치가쯔돈", price:"7,500 -> 6000"},
+			{item:"우동", price:"6,000"},
+			{item:"김치우동", price:"7,000 -> 6,000"},
+			{item:"돌솥알밥", price:"6,500 -> 6,000"},
+			{item:"회덮밥", price:"6,500 -> 6000"},
+			{item:"자루소바", price:"6,000"},
+			{item:"새우튀김", price:"6,000"},
+			{item:"새우튀김우동", price:"6,000"},
+			{item:"타코야끼", price:"6,000"},
+		]
+	},
+	{ 
+		Rid:"6", 
+		title: "Pho커리", 
+		tel:"0317893514", 
+		img:"img/img_phocurry.png", 
+		desc:[
+			{line:"평일 09:00~21:00 / 주말,공휴일 10:00~20:00"}, 
+			{line:"위치: H스퀘어 S동"}, 
+			{line:"메뉴: 돈까스커리, 양지쌀국수, 오무라이스"}
+		],
+		menulist:[
+			{item:"돈까스커리", price:"6,000"},
+			{item:"양지쌀국수", price:"6,000"},
+			{item:"오무라이스", price:"6,000"},
+		]
+	},
+	{ 
+		Rid:"7", 
+		title: "Rotutu", 
+		tel:"0316967455", 
+		img:"img/img_rotutu.png", 
+		desc:[
+			{line:"배달가능 / 평일 10:30~22:00 / 토요일/공휴일 11:00~22:00 / 일요일: 휴무"}, 
+			{line:"위치: H스퀘어 S동"}, 
+			{line:"메뉴: 햄에그샌드위치+음료, 햄치즈샌드위치+음료, 샌드위치: 스테이크/베이컨, 샐러드: 닭가슴살/리코타치즈"}
+		],
+		menulist:[
+			{item:"햄에그샌드위치+음료", price: "6,500 -> 6,000"},
+			{item:"햄치즈샌드위치+음료", price: "6,000"},
+			{item:"스테이크샌드위치", price: "6,500 -> 6,000"},
+			{item:"베이컨샌드위치", price: "6,500 -> 6,000"},
+			{item:"닭가슴살샐러드", price: "8,000 -> 6,000"},
+			{item:"리코타치즈샐러드", price: "7,000 -> 6,000"},
+		]
+	},
+];
+
+App.EatoutRoute = Ember.Route.extend({
+	model: function() {
+		return RestaurantsData;
+	}
+})
 
 App.CafeController = Ember.Controller.extend({
 	selectedMenuCount: null,
@@ -178,6 +366,18 @@ App.ParkingComponent=Ember.Component.extend({
 	}
 });
 
+
+Ember.Handlebars.helper('format-date', function(date){
+	var d = new Date(date);
+	var week = new Array('일', '월', '화', '수', '목', '금', '토');
+	var str=(week[d.getDay()]);
+	return new Ember.Handlebars.SafeString(date+' ('+str+')');
+});
+
+Ember.Handlebars.helper('format-tel', function(tel){
+	var retVal=tel.substr(0,3)+'-'+tel.substr(3,3)+'-'+tel.substr(6,4);
+	return retVal;
+});
 
 function getCookie( name ) {
 	var start = document.cookie.indexOf( name + "=" );
