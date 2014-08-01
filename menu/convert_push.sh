b8767bf505ccab65f8df4edd2e8448e85a7996ef
ikko <me@1kko.com>
2014-08-01 19:47:26 +0900

    Menu Update

--- menu/convert_push.sh
+++ menu/convert_push.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 datapath=.
-/usr/bin/iconv -f euc-kr -t utf-8 $datapath/csv_export.csv > $datapath/csv_export_utf8.csv
+#/usr/bin/iconv -f euc-kr -t utf-8 $datapath/csv_export.csv > $datapath/csv_export_utf8.csv
+/usr/bin/iconv -f euc-kr -t utf-8 csv_export.csv > csv_export_utf8.csv
 #git add $datapath/csv_export_utf8.csv
-git commit -m "Automatic Data Change Commit"
+#git commit -m "Automatic Data Change Commit"
 #git push
 
 
