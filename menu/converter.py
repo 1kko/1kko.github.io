0b4b6a5d392f9900e0fd47393206ffd037e9359f
ikko <me@1kko.com>
2015-05-11 17:22:44 +0900

    test

--- menu/converter.py
+++ menu/converter.py
@@ -367,9 +367,9 @@ if __name__ == '__main__':
 	if len(sys.argv)>=2:
 		filename=sys.argv[1]
 	else:
+		sendEmailFlag=True
 		try:
 			commitFlag=False
-			sendEmailFlag=True
 			tee=Tee()
 
 			tee.write("Fetching from Email")
