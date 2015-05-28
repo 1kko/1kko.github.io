c977908305d8253859c23086021678469d4d3cec
ikko <me@1kko.com>
2015-05-28 18:05:30 +0900

    eat out update

--- js/app.js
+++ js/app.js
@@ -120,121 +120,91 @@ function getMenusNotice(Url) {
 var RestaurantsData=[
 	{
 		Rid:"1", 
-		title: "명인만두", 
-		tel:"0316967433", 
-		img:"img/img_myoungin.png", 
+		title: "미정국수", 
+		tel:"0317893669", 
+		img:"img/img_mijung.png", //add image (128x128).png
 		desc:[
-			{line:"평일 07:30~21:00 / 일요일/공휴일 07:30~19:30 / 토요일: 휴무"}, 
+			{line:"평일/주말/공휴일 09:30~21:30"}, 
 			{line:"위치: H스퀘어"}, 
-			{line:"메뉴: 분식류(6000원 한도)"}
+			{line:"메뉴: 국수류, 주먹밥류"}
 		],
 		menulist:[
-			{item:"고기만두", price:"3,500"}, 
-			{item:"김치만두", price:"3,500"}, 
-			{item:"고기+김치", price:"	3,500"}, 
-			{item:"군만두", price:"4,000"}, 
-			{item:"모듬만두", price:"6,000"}, 
-			{item:"고추만두", price:"4,000"}, 
-			{item:"수제튀김만두", price:"3,000"}, 
-			{item:"떡튀범벅", price:"5,500"}, 
-			{item:"비빔만두", price:"6,000"}, 
-			{item:"(떡)만두국", price:"6,000"},	 
-			{item:"만두칼국수", price:"6,000"}, 
-			{item:"얼큰만두국", price:"6,000"}, 
-			{item:"만두잡채덮밥", price:"6,000"}, 
-			{item:"고추김밥", price:"3,500"}, 
-			{item:"새우김밥", price:"3,500"}, 
-			{item:"참치김밥", price:"3,500"}, 
-			{item:"모듬김밥", price:"3,500"}, 
-			{item:"야채김밥", price:"2,500"}, 
-			{item:"치즈김밥", price:"3,000"}, 
-			{item:"오뎅(1인분", price:"2,000"}, 
-			{item:"명인떡볶이", price:"3,500"}, 
-			{item:"라면사리,오뎅, 계란, 만두", price:"각 1,000"}, 
-			{item:"항아리수제비", price:"6,000"}, 
-			{item:"짜장라볶이", price:"4,500"}, 
-			{item:"해물볶음우동", price:"6500 ->6,000"},
-			{item:"비빔국수", price:"5,500"}, 
-			{item:"명인라면", price:"3,000"}, 
-			{item:"떡사리, 만두, 김치, 치즈", price:"각 500"},
-			{item:"해장라면", price:"4,500"}, 
-			{item:"물냉면", price:"5,000"}, 
-			{item:"비빔냉면", price:"5,000"}, 
-			{item:"쫄면", price:"	5,000"}, 
-			{item:"해물오믈렛", price:"6,000"}, 
-			{item:"김치새우볶음밥", price:"6,000"}, 
-			{item:"김치참치볶음밥", price:"6,000"}, 
-			{item:"명인볶음밥", price:"6,000"}, 
-			{item:"김치(참치)찌개", price:"5,000"},	
-			{item:"순두부찌개", price:"5,000"}, 
-			{item:"돈까스", price:"6,000"}, 
-			{item:"매콤치킨까스", price:"6,500 -> 6,000"},
-			{item:"고구마치즈돈까스", price:"6,500 -> 6,000"},
-			{item:"된장찌개", price:"5,000"}, 
-			{item:"제육덮밥", price:"6,500 -> 6,000 "},
-			{item:"오징어덮밥", price:"6,000"}, 
-			{item:"비빔밥", price:"5,000"}, 
-			{item:"돌솥비빔밥", price:"6,000"}, 
-			{item:"김치날치알돌솥밥", price:"6,000"}, 
-			{item:"공기밥", price:"1,000"}, 
-			{item:"모닝SET(오전11시까지) 라면+김밥+계란후라이", price:"5,000"},
-			{item:"냉쫄면", price:"5,000"}, 
-			{item:"냉모밀만두정식", price:"6,000"}, 
-			{item:"콩국수", price:"6,000"},
+			{item:"set:국수(기본 택1)+제육주먹밥", price:"6,500"},
+			{item:"set:국수(Extra 택1)+미정주먹밥", price:"6,500"},
+			{item:"set:멸치국수+고추기름물만두", price:"-"},
+			{item:"냉국수(기본/Extra)", price:"4,000"},
+			{item:"비빔국수(기본/Extra)", price:"4,000"},
+			{item:"간비국수(간장비빔국수)(기본/extra)", price:"4,000"},
+			{item:"얼큰멸치국수(기본/Extra)", price:"4,000"},
+			{item:"만두국수(기본/Extra)", price:"4,000"},
+			{item:"냉콩국수(기본/Extra)-여름", price:"5,000"},
+			{item:"제육덮밥", price:"-"},
+			{item:"고추기름물만두", price:"4000"},
+			{item:"제육주먹밥", price:"2,500"},
+			{item:"미정주먹밥", price:"1,500"}
 		]
 	},
 	{ 
-		Rid:"2", 
-		title: "찌개애감동", 
-		tel:"0316281479", 
-		img:"img/img_jjigae.png", 
+		Rid:"2",
+		title: "소공동뚝배기", 
+		tel:"0316984548", 
+		img:"img/img_sogongdong.png", //add image
 		desc:[
-			{line:"평일 10:30~22:00 / 토요일/공휴일 10:30~15:00 / 일요일: 휴무"}, 
-			{line:"위치: 유스페이스"},
-			{line:"메뉴: 찌개류, 제육불고기"}
+			{line:"평일/주말/공휴일 09:00~21:00"}, 
+			{line:"위치: 삼환하이팩스 "},
+			{line:"메뉴: 순두부, 찌개류 (1인당 7,000원 한도)"}
 		],
 		menulist:[
-			{item:"차돌박이된장찌개", price:"7,000 -> 6,000"},
-			{item:"시골된장찌개", price:"6,000"},
-			{item:"옛날청국장찌개", price:"6,000"},
-			{item:"해물순두부찌개", price:"6,000"},
-			{item:"추억愛고추장찌개", price:"6,000"},
-			{item:"참치김치찌개", price:"6,000"},
-			{item:"옹기비빔밥", price:"6,000"},
-			{item:"뚝불고기정식", price:"7,000 -> 6,000"},
-			{item:"모짜렐라치즈계란말이", price:"6,000"},
-			{item:"계란탕", price:"6,000"},
-			{item:"제육불고기", price:"6,000"},
-			{item:"생선구이(단품)", price:"6,000"},
+			{item:"1962원조 순두부", price:"6,000"},
+			{item:"해장순두부", price:"7,000"},
+			{item:"버섯순두부", price:"7,000"},
+			{item:"만두순두부", price:"7,000"},
+			{item:"햄 순두부", price:"7,000"},
+			{item:"치즈순두부", price:"7,000"},
+			{item:"콩비지 뚝배기", price:"6,000"},
+			{item:"청국장 뚝배기", price:"6,000"}
 		]
 	},
 	{ 
-		Rid:"3", 
+		Rid:"3",
+		title: "마미밥", 
+		tel:"0316987827", 
+		img:"img/img_mommybab.png", //add image
+		desc:[
+			{line:"평일 08:00~20:30 / 주말, 공휴일 운영 없음"}, 
+			{line:"위치: 삼환하이팩스"}, 
+			{line:"메뉴: 집밥류"}
+		],
+		menulist:[
+			{item:"현미김밥", price:"-"},
+			{item:"매콤견과김밥", price:"-"},
+			{item:"참치김밥", price:"-"},
+			{item:"낙지덮밥", price:"-"},
+			{item:"단호박죽", price:"-"},
+			{item:"얼큰해장라면", price:"-"},
+			{item:"감자샐러드", price:"-"},
+			{item:"새우튀김", price:"-"},
+			{item:"열무비빔국수", price:"-"},
+			{item:"어묵탕(겨울메뉴)", price:"-"},
+			{item:"소불고기덮밥(월)", price:"-"},
+			{item:"어묵우동(겨울메뉴)", price:"-"},
+			{item:"김치볶음밥(목)", price:"-"},
+			{item:"삼겹살김치찌개(화)", price:"-"}
+		]
+	},
+	{ 
+		Rid:"4", 
 		title: "담소사골순대", 
 		tel:"0317893821", 
 		img:"img/img_damso.png", 
 		desc:[
 			{line:"평일 24시간 / 월요일 11:00 Open / 일요일: 22:30 Close"}, 
 			{line:"위치: H스퀘어"}, 
-			{line:"메뉴: 메뉴: 우사골돈순대국\우사골우순대국"}
+			{line:"메뉴: 우사골돈순대국/우사골우순대국"}
 		],
 		menulist:[
 			{item:"우사골돈순대국", price:"5,700"},
-			{item:"우사골우순대국", price:"5,900"},
-		]
-	},
-	{ 
-		Rid:"4", 
-		title: "the진국", 
-		tel:"0317893555", 
-		img:"img/img_jincook.png", 
-		desc:[
-			{line:"평일/토요일/공휴일 24시간 / 일요일: 22:30 Close"}, 
-			{line:"위치: H스퀘어 S동"}, 
-			{line:"메뉴: 수육국밥"}
-		],
-		menulist:[
-			{item:"수육국밥", price:"6,000"}, 	
+			{item:"우사골우순대국", price:"5,900"}
 		]
 	},
 	{ 
@@ -257,7 +227,7 @@ var RestaurantsData=[
 			{item:"자루소바", price:"6,000"},
 			{item:"새우튀김", price:"6,000"},
 			{item:"새우튀김우동", price:"6,000"},
-			{item:"타코야끼", price:"6,000"},
+			{item:"타코야끼", price:"6,000"}
 		]
 	},
 	{ 
@@ -273,7 +243,7 @@ var RestaurantsData=[
 		menulist:[
 			{item:"돈까스커리", price:"6,000"},
 			{item:"양지쌀국수", price:"6,000"},
-			{item:"오무라이스", price:"6,000"},
+			{item:"오무라이스", price:"6,000"}
 		]
 	},
 	{ 
@@ -292,7 +262,7 @@ var RestaurantsData=[
 			{item:"스테이크샌드위치", price: "6,500 -> 6,000"},
 			{item:"베이컨샌드위치", price: "6,500 -> 6,000"},
 			{item:"닭가슴살샐러드", price: "8,000 -> 6,000"},
-			{item:"리코타치즈샐러드", price: "7,000 -> 6,000"},
+			{item:"리코타치즈샐러드", price: "7,000 -> 6,000"}
 		]
 	},
 ];
